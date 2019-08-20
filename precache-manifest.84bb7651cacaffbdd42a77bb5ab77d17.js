e26aac9fa2f1ba1daa0af2ce86a84c09d59346bd
cvillafraz <ciro1200@gmail.com>
2019-08-20 00:59:57 -0400

    Updates

---
+++ precache-manifest.84bb7651cacaffbdd42a77bb5ab77d17.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e7c473c97790f68fdee17a19cc999a8",
+    "url": "/blog-platzi-redux/index.html"
+  },
+  {
+    "revision": "56ffd40881a74bb11449",
+    "url": "/blog-platzi-redux/static/css/2.c63a6383.chunk.css"
+  },
+  {
+    "revision": "71692648c719c9cb4900",
+    "url": "/blog-platzi-redux/static/css/main.e2e5e45a.chunk.css"
+  },
+  {
+    "revision": "56ffd40881a74bb11449",
+    "url": "/blog-platzi-redux/static/js/2.d171fc2f.chunk.js"
+  },
+  {
+    "revision": "71692648c719c9cb4900",
+    "url": "/blog-platzi-redux/static/js/main.7fd14bcc.chunk.js"
+  },
+  {
+    "revision": "1a1f63a00e78d6bfffd2",
+    "url": "/blog-platzi-redux/static/js/runtime~main.9436e344.js"
+  },
+  {
+    "revision": "03783c5172ee1ad128c576bf88fac168",
+    "url": "/blog-platzi-redux/static/media/fa-brands-400.03783c51.eot"
+  },
+  {
+    "revision": "073c2f3ce60eaf69cc2767ef3d989078",
+    "url": "/blog-platzi-redux/static/media/fa-brands-400.073c2f3c.svg"
+  },
+  {
+    "revision": "7559b3774a0625e8ca6c0160f8f6cfd8",
+    "url": "/blog-platzi-redux/static/media/fa-brands-400.7559b377.woff2"
+  },
+  {
+    "revision": "ed2b8bf117160466ba6220a8f1da54a4",
+    "url": "/blog-platzi-redux/static/media/fa-brands-400.ed2b8bf1.ttf"
+  },
+  {
+    "revision": "fe9d62e0d16a333a20e63c3e7595f82e",
+    "url": "/blog-platzi-redux/static/media/fa-brands-400.fe9d62e0.woff"
+  },
+  {
+    "revision": "59215032a4397507b80e5625dc323de3",
+    "url": "/blog-platzi-redux/static/media/fa-regular-400.59215032.ttf"
+  },
+  {
+    "revision": "8fdea4e89ac405d9f8db327adb331d8d",
+    "url": "/blog-platzi-redux/static/media/fa-regular-400.8fdea4e8.svg"
+  },
+  {
+    "revision": "e07d9e40b26048d9abe2ef966cd6e263",
+    "url": "/blog-platzi-redux/static/media/fa-regular-400.e07d9e40.woff2"
+  },
+  {
+    "revision": "e5770f9863963fb576942e25214a226d",
+    "url": "/blog-platzi-redux/static/media/fa-regular-400.e5770f98.woff"
+  },
+  {
+    "revision": "fc9c63c8224fb341fc933641cbdd12ef",
+    "url": "/blog-platzi-redux/static/media/fa-regular-400.fc9c63c8.eot"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "/blog-platzi-redux/static/media/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "/blog-platzi-redux/static/media/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "b557f56e367e59344ca95f9d1fb44352",
+    "url": "/blog-platzi-redux/static/media/fa-solid-900.b557f56e.svg"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "/blog-platzi-redux/static/media/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "/blog-platzi-redux/static/media/fa-solid-900.ef3df984.eot"
+  }
+]);
